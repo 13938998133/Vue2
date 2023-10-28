148fb79dbe70dc858c0733f214e2df3114cda7ff
13938998133 <71955115+13938998133@users.noreply.github.com>
2023-10-28 17:34:16 +0800

    Add files via upload

---
+++ vue_basic/19、单文件组件/School.vue
@@ -0,0 +1,35 @@
+<!-- 组件的结构 -->
+<template>
+  <div>
+    <h2>学校：{{ name }}</h2>
+    <h2>地址：{{ address }}</h2>
+    <button @click="showName">点我提示学校名</button>
+  </div>
+</template>
+
+
+<!-- 组件交互相关的代码 -->
+<script>
+export default {
+  name: "School",
+  data() {
+    return {
+      name: "尚硅谷",
+      address: "北京",
+    };
+  },
+  methods: {
+    showName() {
+      alert(this.name);
+    },
+  },
+};
+</script>
+
+
+<!-- 组件的样式 -->
+<style lang="scss" scoped>
+.demo {
+  background-color: orange;
+}
+</style>
---
+++ vue_basic/19、单文件组件/Student.vue
@@ -0,0 +1,21 @@
+<!-- 组件的结构 -->
+<template>
+  <div>
+    <h2>学生姓名：{{ name }}</h2>
+    <h2>学生年龄：{{ age }}</h2>
+  </div>
+</template>
+
+
+<!-- 组件交互相关的代码 -->
+<script>
+export default {
+  name: "Student",
+  data() {
+    return {
+      name: "张三",
+      age: 19,
+    };
+  },
+};
+</script>
